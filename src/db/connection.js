95cd286185853eaa5f4c7870282c12f970b97a6e
caiooliveirah09 <leelros@hotmail.com>
2022-10-21 15:44:14 -0300

    test2

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'localhost',
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
 
